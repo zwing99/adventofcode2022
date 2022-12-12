dd715b55e82130ed8fed812f36bddc22d3fc0b55
Zac Oler <zacoler@granular.ag>
2022-12-11 18:41:00 -0600

    foobar

--- oldyears4practice/2021/day6/solve.py
+++ oldyears4practice/2021/day6/solve.py
@@ -5,54 +5,36 @@ filename = sys.argv[1] if len(sys.argv) > 1 else 'input.txt'
 with open(filename) as fh:
     lines = [line.strip() for line in fh.readlines()]
 
-fishes = [int(x) for x in lines[0].split(",")]
-for i in range(80):
-    new_list = []
-    extended = []
-    for fish in fishes:
-        if fish > 0:
-            new_list.append(fish-1)
+fish_counts = {i:0 for i in range(0,9)}
+for x in lines[0].split(","):
+    fish_counts[int(x)] += 1
+
+# part 1
+for j in range(80):
+    new_counts = {i:0 for i in range(0,9)}
+    for i in range(0,9):
+        if i > 0:
+            new_counts[i-1] += fish_counts[i]
         else:
-            new_list.append(6)
-            extended.append(8)
-    fishes = new_list + extended
+            new_counts[6] += fish_counts[i]
+            new_counts[8] += fish_counts[i]
+    fish_counts = new_counts
 
-print(len(fishes))
+print(sum(fish_counts.values()))
 
-fishes = [int(x) for x in lines[0].split(",")]
-total = 0
-for fish in fishes:
-    asdf = [fish]
-    for i in range(80):
-        new_list = []
-        extended = []
-        for f in asdf:
-            if f > 0:
-                new_list.append(f-1)
-            else:
-                new_list.append(6)
-                extended.append(8)
-        asdf = new_list + extended
-        print(len(asdf))
-    print(fish)
-    print(len(asdf))
-    total += len(asdf)
-print(total)
-
-exit(0)
-            
 # part 2
-fishes = [int(x) for x in lines[0].split(",")]
-for i in range(256):
-    print (i)
-    new_list = []
-    extended = []
-    for fish in fishes:
-        if fish > 0:
-            new_list.append(fish-1)
-        else:
-            new_list.append(6)
-            extended.append(8)
-    fishes = new_list + extended
+fish_counts = {i:0 for i in range(0,9)}
+for x in lines[0].split(","):
+    fish_counts[int(x)] += 1
 
-print(len(fishes))
+for j in range(256):
+    new_counts = {i:0 for i in range(0,9)}
+    for i in range(0,9):
+        if i > 0:
+            new_counts[i-1] += fish_counts[i]
+        else:
+            new_counts[6] += fish_counts[i]
+            new_counts[8] += fish_counts[i]
+    fish_counts = new_counts
+
+print(sum(fish_counts.values()))
